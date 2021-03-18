becf0e7dc79feb5800211f0364c40cf693888c7a
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-03-18 13:58:25 -0300

    Update Client.cpp

--- src/Client.cpp
+++ src/Client.cpp
@@ -18,14 +18,46 @@ Client::Client(char *clientName, char *listOfFollowers, int numberOfAccess){
 
 void Client::do_threadSender(void* arg){
     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
+    char *ch;
+    ch = (char*)malloc(1 * sizeof(char)); //pois é, único jeito que to conseguindo é alocar memória para um caractere apenas. Vou ter que olhar um jeito melhor depois.
+	//char **message;
+	//message = (char**) malloc(MAX_NAME_SIZE * sizeof(char*));
+	//message[0] = (char*)malloc(MAX_NAME_SIZE * sizeof(char));
+    int characters = 0;
+    char* message;
+    message = (char*)malloc(MAX_NAME_SIZE * sizeof(char));
+    int i = 0;
+    //int j = 0;	
     while (TRUE) {
         pthread_mutex_lock(&mutex);
         //IN�CIO DA SE��O CR�TICA
+	do {
+		ch[0] = _getch();
+		if (ch[0] == CR) {
+			//j++;
+			cout << endl;
+			//message[j] = (char*)malloc(MAX_NAME_SIZE * sizeof(char));
+		}
+			
+		if (characters <= 130) {
+			_putch(ch[0]);
+			//message[i][j] = ch;
+			//message[i] = ch;
+			message[i] = ch[0];
+			i++;
+			characters++;
+		}
+		
 
+	} while (ch[0] != '@');
+	
+	cout << "\nMensagem enviada:" << (char*)message;
         //FIM DA SE��O CR�TICA
         pthread_mutex_unlock(&mutex);
     }
+	
+	free(message);
+	free(ch);
 }
 
 void Client::do_threadReceiver(void* arg){
