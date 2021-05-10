9913e3bbdd9e2aba16245ad20c2806e0f9262d1d
lucashagemeister <80707197+lucashagemeister@users.noreply.github.com>
2021-05-10 19:13:35 -0300

    Update config.hpp

--- include/config.hpp
+++ include/config.hpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <List>
+#include <map>
 
+class Config{
+  public:
+    map<string, string> possibleServerAddresses;
+    int generateIpsAndGatesLists();
+  
+}  
+  
